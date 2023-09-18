ba4e7327bfac11c544afa531fbd78dae8f1e9836
andrejz2010 <zacharovandrej1@gmail.com>
2023-09-18 14:55:16 +0100

    fizz buzz

--- app/src/main/java/fizzbuzz/App.java
+++ app/src/main/java/fizzbuzz/App.java
@@ -5,7 +5,21 @@ package fizzbuzz;
 
 public class App {
     public static void fizzbuzz() {
-        // Your code goes here!
+
+        for (int i = 1; i <= 100; i++) {
+String result = "";
+  if (i % 5 == 0 && i % 3 == 0){
+    result="fizzbuzz";
+} else if (i%3==0){
+    result+="fizz";
+} else if (i % 5 == 0) {
+      result+="buzz";
+  } else {
+    result+=i;
+}
+            System.out.println(result);
+
+        }
     }
 
     public static void main(String[] args) {
